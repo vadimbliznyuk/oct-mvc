5285bb5b389adbe1359d31b8652376f356a1ddfe
Vasyl <admin@gmail.com>
2019-04-23 19:37:25 +0300

    add view and model propertis

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,5 +3,14 @@
 namespace core;
 
 abstract class Controller {
+    
+    protected $model;
+    protected $view;
+    
+    public function __construct() {
+	$this->view = new Views();
+    }
+
+
     abstract public function action_index();
 }
