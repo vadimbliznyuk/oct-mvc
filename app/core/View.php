9a307f24b596d66a9cb1ac05fbb1ff5d50bdde29
Vasyl <admin@gmail.com>
2019-04-23 19:41:12 +0300

    add render

--- app/core/View.php
+++ app/core/View.php
@@ -3,5 +3,9 @@
 namespace core;
 
 class Views {
-    //put your code here
+
+    public function render($content_view, $template_view = 'template_main_view') {
+	include_once 'app'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.$template_view.'.php';
+    }
+
 }
