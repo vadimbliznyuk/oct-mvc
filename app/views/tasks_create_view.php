1231b9da64c048cb2cdf4db106f20b0e051dc8b8
vadimbliznyuk <vadim.bliznyuk86@gmail.com>
2019-04-29 14:05:34 +0300

    Добавленно поле причина отмены Исправлена валидация полей дата отмены и дата активации фактическая Добавлено ФИО тех представителя, дата отмены и причина отмены в карточку клиента

--- app/views/tasks_create_view.php
+++ app/views/tasks_create_view.php
@@ -2,7 +2,7 @@
     <h2>Добавить задачу</h2>
 </div>
 <?php if (!empty($this->error)): ?>
-    <div class="w3-panel w3-pale-red w3-display-container w3-border w3-margin"><?= $this->error ?></div>
+    <div class="w3-panel w3-pale-red w3-display-container w3-border"><?= $this->error ?></div>
 <?php endif; ?>
 <form  method="POST" action="/tasks/add">
     <input class="w3-input w3-border w3-light-grey w3-section" type="text" name="tasks">
